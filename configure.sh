40fec57aa509fdd8c4cab0f565bebad45e955880
Kernel114514 <kernel114514@hotmail.com>
2025-07-25 19:20:46 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -28,6 +28,6 @@ sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resourc
 #install ngrok
 cd ~
 wget https://drive.mcsl.com.cn/d/ME-Frp/Natayark/MEFrpc/0.61.1/mefrpc_darwin_arm64_0.61.1.tar
-tar -xvhf mefrpc_darwin_arm64_0.61.1.tar
+tar xf mefrpc_darwin_arm64_0.61.1.tar
 cd *me* | true
 ./mefrpc -t 31ef16fdcf01c7dd9774c88924b80d88 -p 95922
